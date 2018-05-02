187e7a1783ffff5d700e973c87dbc6165ff2dcd9
Jonathan Kinred <jkinred@linuxfoundation.org>
2018-05-02 21:32:41 +1000

    Implement conditional logic for role creation
    This allows the advertised variables to function.
    
    The outputs had to be modified to work with conditionals in line with
    the workaround in https://github.com/hashicorp/terraform/issues/17425.

--- modules/iam-assumable-roles/main.tf
+++ modules/iam-assumable-roles/main.tf
@@ -38,6 +38,8 @@ data "aws_iam_policy_document" "assume_role_with_mfa" {
 
 # Admin
 resource "aws_iam_role" "admin" {
+  count = "${var.create_admin_role ? 1 : 0}"
+
   name = "${var.admin_role_name}"
   path = "${var.admin_role_path}"
 
@@ -45,17 +47,23 @@ resource "aws_iam_role" "admin" {
 }
 
 resource "aws_iam_role_policy_attachment" "admin" {
+  count = "${var.create_admin_role ? 1 : 0}"
+
   role       = "${aws_iam_role.admin.name}"
   policy_arn = "${var.admin_role_policy_arn}"
 }
 
 # Poweruser
 resource "aws_iam_role_policy_attachment" "poweruser" {
+  count = "${var.create_poweruser_role ? 1 : 0}"
+
   role       = "${aws_iam_role.poweruser.name}"
   policy_arn = "${var.poweruser_role_policy_arn}"
 }
 
 resource "aws_iam_role" "poweruser" {
+  count = "${var.create_poweruser_role ? 1 : 0}"
+
   name = "${var.poweruser_role_name}"
   path = "${var.poweruser_role_path}"
 
@@ -64,11 +72,15 @@ resource "aws_iam_role" "poweruser" {
 
 # Readonly
 resource "aws_iam_role_policy_attachment" "readonly" {
+  count = "${var.create_readonly_role ? 1 : 0}"
+
   role       = "${aws_iam_role.readonly.name}"
   policy_arn = "${var.readonly_role_policy_arn}"
 }
 
 resource "aws_iam_role" "readonly" {
+  count = "${var.create_readonly_role ? 1 : 0}"
+
   name = "${var.readonly_role_name}"
   path = "${var.readonly_role_path}"
 
