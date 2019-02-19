fd886b2f6b7b9b40ab96262a595a461c657c0dc6
Ramakanta Routray <13198324+ramakantar@users.noreply.github.com>
2019-02-19 14:11:21 +0100

    Added iam policy (#15)

---
+++ modules/iam-policy/outputs.tf
@@ -0,0 +1,29 @@
+output "id" {
+  description = "The policy's ID"
+  value = "${aws_iam_policy.policy.id}"
+}
+
+output "arn" {
+  description = "The ARN assigned by AWS to this policy"
+  value = "${aws_iam_policy.policy.arn}"
+}
+
+output "description" {
+  description = "The description of the policy"
+  value = "${aws_iam_policy.policy.description}"
+}
+
+output "name" {
+  description = "The name of the policy"
+  value = "${aws_iam_policy.policy.name}"
+}
+
+output "path" {
+  description = "The path of the policy in IAM"
+  value = "${aws_iam_policy.policy.path}"
+}
+
+output "policy" {
+  description = "The policy document"
+  value = "${aws_iam_policy.policy.policy}"
+}
