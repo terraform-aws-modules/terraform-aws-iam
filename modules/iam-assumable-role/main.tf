1bbc7715f457566ef377d2d9b0e19c0889c78d0d
Konstantin Lepikhov <konstantin@tiqets.com>
2020-01-09 09:30:20 +0100

    iam-assumable-role: add description support
    This is based on https://github.com/terraform-aws-modules/terraform-aws-iam/issues/40

--- modules/iam-assumable-role/main.tf
+++ modules/iam-assumable-role/main.tf
@@ -52,6 +52,7 @@ resource "aws_iam_role" "this" {
   name                 = var.role_name
   path                 = var.role_path
   max_session_duration = var.max_session_duration
+  description          = var.role_description
 
   permissions_boundary = var.role_permissions_boundary_arn
 
