699865f63a38dbbfe6e8a520cbeb6e576c5173e9
cdhesse <cdhesse76@gmail.com>
2022-03-24 21:17:22 -0400

    Update modules/iam-role-for-service-accounts-eks/variables.tf
    Co-authored-by: Bryant Biggs <bryantbiggs@gmail.com>

--- modules/iam-role-for-service-accounts-eks/variables.tf
+++ modules/iam-role-for-service-accounts-eks/variables.tf
@@ -181,7 +181,7 @@ variable "attach_load_balancer_controller_policy" {
 # https://kubernetes-sigs.github.io/aws-load-balancer-controller/v2.4/guide/targetgroupbinding/targetgroupbinding/#reference
 # https://kubernetes-sigs.github.io/aws-load-balancer-controller/v2.4/deploy/installation/#setup-iam-manually
 variable "attach_load_balancer_controller_targetgroup_binding_only_policy" {
-  description = "Determines whether to attach the Load Balancer Controller policy for the TargetGroup binding only scenario."
+  description = "Determines whether to attach the Load Balancer Controller policy for the TargetGroupBinding only"
   type        = bool
   default     = false
 }
