19fbeabb88d8a4ad10598fd09d3065ff03539efe
Xin Chen <chenxin@konvery.com>
2022-04-18 20:31:41 +0800

    Add aud condition to sa

--- modules/iam-role-for-service-accounts-eks/main.tf
+++ modules/iam-role-for-service-accounts-eks/main.tf
@@ -21,7 +21,7 @@ data "aws_iam_policy_document" "this" {
 
       condition {
         test     = var.assume_role_condition_test
-        variable = "${replace(statement.value.provider_arn, "/^(.*provider/)/", "")}:sub"
+        variable = "${replace(statement.value.provider_arn, "/^(.*provider/)/", "")}:aud:sub"
         values   = [for sa in statement.value.namespace_service_accounts : "system:serviceaccount:${sa}"]
       }
     }
