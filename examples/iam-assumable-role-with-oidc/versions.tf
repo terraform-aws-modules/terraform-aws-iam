d431be077c56e917726ae1edf16a155aaf889c6b
theprashantyadav <theprashantyadav7@gmail.com>
2022-05-11 15:30:54 +0530

    update versions.tf & update github-action

--- examples/iam-assumable-role-with-oidc/versions.tf
+++ examples/iam-assumable-role-with-oidc/versions.tf
@@ -1,7 +1,11 @@
+# Terraform version
 terraform {
-  required_version = ">= 0.12.6"
+  required_version = ">= 0.14.0"
 
   required_providers {
-    aws = ">= 2.23"
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.1.15"
+    }
   }
 }
