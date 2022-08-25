358f7d438d033df9f463b518ef229333f1027bf6
Nikolay Kolev <nikolay@users.noreply.github.com>
2022-08-25 02:48:52 -0700

    fix: Don't force users to reset passwords in modules/iam-user (#271)

--- modules/iam-user/main.tf
+++ modules/iam-user/main.tf
@@ -16,6 +16,11 @@ resource "aws_iam_user_login_profile" "this" {
   pgp_key                 = var.pgp_key
   password_length         = var.password_length
   password_reset_required = var.password_reset_required
+
+  # TODO: Remove once https://github.com/hashicorp/terraform-provider-aws/issues/23567 is resolved
+  lifecycle {
+    ignore_changes = [password_reset_required]
+  }
 }
 
 resource "aws_iam_access_key" "this" {
