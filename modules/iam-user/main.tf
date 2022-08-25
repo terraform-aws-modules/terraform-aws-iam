c3c1c54ae3b3be9d440ef0113a0123e3e92fe3b6
Nikolay Kolev <nikolay@users.noreply.github.com>
2022-08-25 01:52:04 -0700

    docs: Link the upstream issue to the workaround

--- modules/iam-user/main.tf
+++ modules/iam-user/main.tf
@@ -17,6 +17,7 @@ resource "aws_iam_user_login_profile" "this" {
   password_length         = var.password_length
   password_reset_required = var.password_reset_required
 
+  # TODO: Remove once https://github.com/hashicorp/terraform-provider-aws/issues/23567 is resolved
   lifecycle {
     ignore_changes = [password_reset_required]
   }
