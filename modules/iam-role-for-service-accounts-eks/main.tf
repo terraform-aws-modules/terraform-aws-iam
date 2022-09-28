6ce1dc4adc73af36c43edf5db01ce4b7aa0f73a2
Fernando Miguel <github@FernandoMiguel.net>
2022-09-28 17:33:12 +0100

    lint check
    Signed-off-by: Fernando Miguel <github@FernandoMiguel.net>

--- modules/iam-role-for-service-accounts-eks/main.tf
+++ modules/iam-role-for-service-accounts-eks/main.tf
@@ -23,7 +23,7 @@ data "aws_iam_policy_document" "this" {
         type        = "AWS"
         identifiers = ["*"]
       }
-      
+
       condition {
         test     = "ArnLike"
         variable = "aws:PrincipalArn"
