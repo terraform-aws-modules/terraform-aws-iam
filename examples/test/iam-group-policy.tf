aa5f46eadae54ba9d905db718269aa97165b7cce
bhgulati <108667614+bhgulati@users.noreply.github.com>
2022-11-01 01:53:54 +0530

    Update iam-group-policy.tf

--- examples/test/iam-group-policy.tf
+++ examples/test/iam-group-policy.tf
@@ -1,1 +1,24 @@
+resource "aws_iam_group_policy" "my_developer_policy" {
+  name  = "my_developer_policy"
+  group = aws_iam_group.my_developers.name
 
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ec2:Describe*",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
+
+resource "aws_iam_group" "my_developers" {
+  name = "developers"
+  path = "/users/"
+}
