9fd3a0a33a737e863fcef29cddae33c2dbf8d8ac
bhgulati <108667614+bhgulati@users.noreply.github.com>
2022-11-11 03:10:08 +0530

    Create Cloudwatch-Alarm.tf

---
+++ examples/test/Cloudwatch-Alarm.tf
@@ -0,0 +1,12 @@
+resource "aws_cloudwatch_metric_alarm" "foobar" {
+  alarm_name                = "terraform-test-foobar5"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = "2"
+  metric_name               = "IAMPolicyEventCount"
+  namespace                 = "AWS/EC2"
+  period                    = "120"
+  statistic                 = "Average"
+  threshold                 = "80"
+  alarm_description         = "This metric monitors ec2 cpu utilization"
+  insufficient_data_actions = []
+}
