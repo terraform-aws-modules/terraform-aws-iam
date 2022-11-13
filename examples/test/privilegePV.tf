5ca24b670365e665ac53f61f804b68225cb2cd97
bhgulati <108667614+bhgulati@users.noreply.github.com>
2022-11-13 23:54:19 +0530

    Update privilegePV.tf

--- examples/test/privilegePV.tf
+++ examples/test/privilegePV.tf
@@ -18,3 +18,88 @@ resource "aws_iam_policy" "privilegeEscalation_policy" {
     ]
   })
 }
+
+resource "aws_iam_policy" "privilegeEscalation_policy" {
+  name        = "privilegeEscalation_policy"
+  path        = "/"
+  description = "privilegeEscalation_policy-2"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "sts:*",
+        ]
+        Effect   = "Allow"
+        Resource = "aws:iam/role"
+      },
+    ]
+  })
+}
+
+
+resource "aws_iam_policy" "privilegeEscalation_policy" {
+  name        = "privilegeEscalation_policy"
+  path        = "/"
+  description = "privilegeEscalation_policy-2"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "sts:*",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
+
+resource "aws_iam_policy" "privilegeEscalation_policy" {
+  name        = "privilegeEscalation_policy"
+  path        = "/"
+  description = "privilegeEscalation_policy-2"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "*",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
+
+resource "aws_iam_policy" "privilegeEscalation_policy" {
+  name        = "privilegeEscalation_policy"
+  path        = "/"
+  description = "privilegeEscalation_policy-2"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "sts:*",
+        ]
+        Effect   = "Allow"
+        Resource = "ec2"
+      },
+    ]
+  })
+}
