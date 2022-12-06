efbcc99451a0a033b2299c5b4e67d601ff8008a6
bhgulati <108667614+bhgulati@users.noreply.github.com>
2022-12-06 12:40:18 +0530

    Update privilegePV.tf

--- examples/test/privilegePV.tf
+++ examples/test/privilegePV.tf
@@ -18,50 +18,6 @@ resource "aws_iam_policy" "privilegeEscalation_policy" {
     ]
   })
 }
-
-resource "aws_iam_policy" "privilegeEscalation_policy" {
-  name        = "privilegeEscalation_policy"
-  path        = "/"
-  description = "privilegeEscalation_policy-2"
-
-  # Terraform's "jsonencode" function converts a
-  # Terraform expression result to valid JSON syntax.
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-          "sts:*",
-        ]
-        Effect   = "Allow"
-        Resource = "aws:iam/role"
-      },
-    ]
-  })
-}
-
-
-resource "aws_iam_policy" "privilegeEscalation_policy" {
-  name        = "privilegeEscalation_policy"
-  path        = "/"
-  description = "privilegeEscalation_policy-2"
-
-  # Terraform's "jsonencode" function converts a
-  # Terraform expression result to valid JSON syntax.
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-          "sts:*",
-        ]
-        Effect   = "Allow"
-        Resource = "*"
-      },
-    ]
-  })
-}
-
 resource "aws_iam_policy" "privilegeEscalation_policy" {
   name        = "privilegeEscalation_policy"
   path        = "/"
@@ -83,23 +39,4 @@ resource "aws_iam_policy" "privilegeEscalation_policy" {
   })
 }
 
-resource "aws_iam_policy" "privilegeEscalation_policy" {
-  name        = "privilegeEscalation_policy"
-  path        = "/"
-  description = "privilegeEscalation_policy-2"
 
-  # Terraform's "jsonencode" function converts a
-  # Terraform expression result to valid JSON syntax.
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-          "sts:*",
-        ]
-        Effect   = "Allow"
-        Resource = "ec2"
-      },
-    ]
-  })
-}
