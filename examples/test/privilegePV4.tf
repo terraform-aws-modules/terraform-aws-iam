2181495ff30afcf98dcfbc6de3db2b0b1e5a1106
bhgulati <108667614+bhgulati@users.noreply.github.com>
2022-12-06 12:40:05 +0530

    Create privilegePV4.tf

---
+++ examples/test/privilegePV4.tf
@@ -0,0 +1,20 @@
+resource "aws_iam_policy" "privilegeEscalation_policy" {
+  name        = "privilegeEscalation_policy"
+  path        = "/"
+  description = "privilegeEscalation_policy-2"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "sts:*",
+        ]
+        Effect   = "Allow"
+        Resource = "ec2"
+      },
+    ]
+  })
+}
