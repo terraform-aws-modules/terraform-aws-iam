4619c5f2dfef9c4ceb65ed3052cf067b137628f2
bhgulati <108667614+bhgulati@users.noreply.github.com>
2022-12-07 21:29:09 +0530

    Create cross-account-access3.tf

---
+++ examples/test/cross-account-access3.tf
@@ -0,0 +1,28 @@
+resource "aws_iam_role" "test_role" {
+  name = "test_role"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          AWS = "ec2.amazonaws.com"
+        Condition = {
+                "BoolIfExists" =  {
+                    "aws:MultiFactorAuthPresent"= "true"
+                }
+            }
+        }
+      },
+    ]
+  })
+
+  tags = {
+    tag-key = "tag-value"
+  }
+}
