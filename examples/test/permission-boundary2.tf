3fc76493c22c03df764a777e28bb8b030204b437
bhgulati <108667614+bhgulati@users.noreply.github.com>
2023-01-08 23:43:10 +0530

    Update permission-boundary2.tf

--- examples/test/permission-boundary2.tf
+++ examples/test/permission-boundary2.tf
@@ -2,5 +2,5 @@ resource "aws_iam_role" "example" {
   name                = "yak_role"
   assume_role_policy  = data.aws_iam_policy_document.instance_assume_role_policy.json # (not shown)
   managed_policy_arns = [aws_iam_policy.policy_one.arn, aws_iam_policy.policy_two.arn]
-  permission_boundary = "aws_iam_policy.policy_one.arn"
+  permission_boundary = aws_iam_policy.policy_one.arn
 }
