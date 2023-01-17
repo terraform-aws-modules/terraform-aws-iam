a0be97cbe69d79e4f653f5c12f1c097fb464a1fd
bhgulati <108667614+bhgulati@users.noreply.github.com>
2023-01-18 02:33:56 +0530

    Create PermissionBoundary-001.tf

---
+++ examples/test/PermissionBoundary-001.tf
@@ -0,0 +1,12 @@
+resource "aws_iam_role" "rolewithoutPB" {
+  name                = "PB_role"
+  assume_role_policy  = data.aws_iam_policy_document.instance_assume_role_policy.json # (not shown)
+  managed_policy_arns = [aws_iam_policy.policy_one.arn, aws_iam_policy.policy_two.arn]
+}
+
+resource "aws_iam_role" "example" {
+  name                = "yak_role"
+  assume_role_policy  = data.aws_iam_policy_document.instance_assume_role_policy.json # (not shown)
+  managed_policy_arns = [aws_iam_policy.policy_one.arn, aws_iam_policy.policy_two.arn]
+  permissions_boundary = [aws_iam_policy.policy_one.arn]
+}
