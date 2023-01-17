55c219b67ec2c1aa228acba1785ef5b173c31d9d
bhgulati <108667614+bhgulati@users.noreply.github.com>
2023-01-18 01:41:59 +0530

    Update Cloudwatch-Alarm.tf

--- examples/test/Cloudwatch-Alarm.tf
+++ examples/test/Cloudwatch-Alarm.tf
@@ -1,7 +1,7 @@
-resource "aws_cloudwatch_metric_alarm" "foobar" {
-  alarm_name                = "terraform-test-foobar5"
+resource "aws_cloudwatch_metric_alarm" "foo" {
+  alarm_name                = "terraform-test-foo5"
   comparison_operator       = "GreaterThanOrEqualToThreshold"
-  evaluation_periods        = "2"
+  evaluation_periods        = "5"
   metric_name               = "IAMPolicyEventCount"
   namespace                 = "AWS/EC2"
   period                    = "120"
