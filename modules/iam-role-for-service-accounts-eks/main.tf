0e0e5443954886f0cee3fceb376bed975631ce31
Guilherme Thomazi Bonicontro <thomazi@linux.com>
2023-02-22 10:04:03 +0100

    feat: Add aws_region datasource in iam-role-for-service-accounts-eks

--- modules/iam-role-for-service-accounts-eks/main.tf
+++ modules/iam-role-for-service-accounts-eks/main.tf
@@ -1,8 +1,10 @@
 data "aws_partition" "current" {}
 data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
 
 locals {
   account_id          = data.aws_caller_identity.current.account_id
+  region              = data.aws_region.current.name
   partition           = data.aws_partition.current.partition
   dns_suffix          = data.aws_partition.current.dns_suffix
   role_name_condition = var.role_name != null ? var.role_name : "${var.role_name_prefix}*"
