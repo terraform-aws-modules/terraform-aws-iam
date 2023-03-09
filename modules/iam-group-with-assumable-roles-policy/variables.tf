d885487d367b9a2a833cabc5f9102a2a9a09781a
Luca Lesinigo <luca@lesinigo.it>
2023-03-09 17:07:25 +0100

    feat: Support for non-default IAM paths in iam-group-with-assumable-roles-policy (#344)

--- modules/iam-group-with-assumable-roles-policy/variables.tf
+++ modules/iam-group-with-assumable-roles-policy/variables.tf
@@ -3,6 +3,19 @@ variable "name" {
   type        = string
 }
 
+variable "path" {
+  description = "Path of IAM policy and IAM group"
+  type        = string
+  default     = "/"
+  validation {
+    condition = alltrue([
+      startswith(var.path, "/"),
+      endswith(var.path, "/"),
+    ])
+    error_message = "IAM path must include leading and trailing slashes"
+  }
+}
+
 variable "assumable_roles" {
   description = "List of IAM roles ARNs which can be assumed by the group"
   type        = list(string)
