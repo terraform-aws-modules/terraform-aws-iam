3fdc1e452aa6201f0ad818260788ac853e503e39
k.bigwheel (kazufumi nishida) <k.bigwheel+eng@gmail.com>
2023-03-16 14:18:59 +0900

    Fix indent

--- modules/iam-github-oidc-provider/main.tf
+++ modules/iam-github-oidc-provider/main.tf
@@ -13,8 +13,8 @@ data "tls_certificate" "this" {
 resource "aws_iam_openid_connect_provider" "this" {
   count = var.create ? 1 : 0
 
-  url             = var.url
-  client_id_list  = coalescelist(var.client_id_list, ["sts.${data.aws_partition.current.dns_suffix}"])
+  url            = var.url
+  client_id_list = coalescelist(var.client_id_list, ["sts.${data.aws_partition.current.dns_suffix}"])
   thumbprint_list = concat(
     data.tls_certificate.this[0].certificates[*].sha1_fingerprint,
     ["6938fd4d98bab03faadb97b34396831e3780aea1"], # https://github.blog/changelog/2022-01-13-github-actions-update-on-oidc-based-deployments-to-aws/
