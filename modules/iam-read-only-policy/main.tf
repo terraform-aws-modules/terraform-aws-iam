5bf5f6f7e539db6c9bd373370613a78dcf2d79ac
Melissa Greenbaum <69476188+magreenbaum@users.noreply.github.com>
2023-04-08 13:57:35 -0400

    feat: Add `name_prefix` to iam-policy and iam-read-only-policy modules (#369)
    Co-authored-by: magreenbaum <magreenbaum>

--- modules/iam-read-only-policy/main.tf
+++ modules/iam-read-only-policy/main.tf
@@ -2,6 +2,7 @@ resource "aws_iam_policy" "policy" {
   count = var.create_policy ? 1 : 0
 
   name        = var.name
+  name_prefix = var.name_prefix
   path        = var.path
   description = var.description
 
