1ac5d0e1a03be6fa19c8c2d8d13eb80fa06f151a
Jason D'Amour <jasondamour98@gmail.com>
2023-04-10 21:33:02 -0700

    fix: count reference

--- modules/iam-user/main.tf
+++ modules/iam-user/main.tf
@@ -19,7 +19,7 @@ resource "aws_iam_user_login_profile" "this" {
   count = var.create_user && var.create_iam_user_login_profile ? 1 : 0
 
   user                    = aws_iam_user.this[0].name
-  pgp_key                 = startswith(var.pgp_key, "http") ? data.http.pgp_key.response_body : var.pgp_key
+  pgp_key                 = startswith(var.pgp_key, "http") ? data.http.pgp_key[0].response_body : var.pgp_key
   password_length         = var.password_length
   password_reset_required = var.password_reset_required
 
