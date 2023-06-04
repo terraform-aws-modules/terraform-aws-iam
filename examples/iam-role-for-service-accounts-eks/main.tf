7e7ad04cbcded5f31ef018bff3bedab81571a7d7
Mossaab Stiri <mstiri@users.noreply.github.com>
2023-06-04 15:45:41 +0200

    :wrench: Add an example to showcase the fixed issue

--- examples/iam-role-for-service-accounts-eks/main.tf
+++ examples/iam-role-for-service-accounts-eks/main.tf
@@ -85,6 +85,24 @@ module "cert_manager_irsa_role" {
   tags = local.tags
 }
 
+module "cert_manager_irsa_role_self_assume" {
+  source = "../../modules/iam-role-for-service-accounts-eks"
+
+  role_name                     = "cert-manager"
+  attach_cert_manager_policy    = true
+  cert_manager_hosted_zone_arns = ["arn:aws:route53:::hostedzone/IClearlyMadeThisUp"]
+  allow_self_assume_role        = true
+
+  oidc_providers = {
+    ex = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:cert-manager"]
+    }
+  }
+
+  tags = local.tags
+}
+
 module "cluster_autoscaler_irsa_role" {
   source = "../../modules/iam-role-for-service-accounts-eks"
 
