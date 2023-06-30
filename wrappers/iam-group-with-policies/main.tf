ada806ea66020d1c460edad8be2f473858ef0f4f
SHINICHIRO SUZUKI <shin@sforzando.co.jp>
2023-06-30 12:09:13 +0900

    fix: :bug: typo "enforcment"
    It should be "enforcement"

--- wrappers/iam-group-with-policies/main.tf
+++ wrappers/iam-group-with-policies/main.tf
@@ -9,7 +9,7 @@ module "wrapper" {
   group_users                            = try(each.value.group_users, var.defaults.group_users, [])
   custom_group_policy_arns               = try(each.value.custom_group_policy_arns, var.defaults.custom_group_policy_arns, [])
   custom_group_policies                  = try(each.value.custom_group_policies, var.defaults.custom_group_policies, [])
-  enable_mfa_enforcment                  = try(each.value.enable_mfa_enforcment, var.defaults.enable_mfa_enforcment, true)
+  enable_mfa_enforcement                  = try(each.value.enable_mfa_enforcement, var.defaults.enable_mfa_enforcement, true)
   attach_iam_self_management_policy      = try(each.value.attach_iam_self_management_policy, var.defaults.attach_iam_self_management_policy, true)
   iam_self_management_policy_name_prefix = try(each.value.iam_self_management_policy_name_prefix, var.defaults.iam_self_management_policy_name_prefix, "IAMSelfManagement-")
   aws_account_id                         = try(each.value.aws_account_id, var.defaults.aws_account_id, "")
