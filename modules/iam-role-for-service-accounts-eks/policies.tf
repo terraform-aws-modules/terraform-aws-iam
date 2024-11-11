9cfab4a2712523308b03acf2d370ea44128b4128
Tim N <1721747+timnee@users.noreply.github.com>
2024-11-11 09:57:29 -0500

    feat: Add `ec2:GetSecurityGroupsForVpc` for AWS LB Controller `v2.10.0` (#536)

--- modules/iam-role-for-service-accounts-eks/policies.tf
+++ modules/iam-role-for-service-accounts-eks/policies.tf
@@ -840,6 +840,7 @@ data "aws_iam_policy_document" "load_balancer_controller" {
       "ec2:DescribeTags",
       "ec2:GetCoipPoolUsage",
       "ec2:DescribeCoipPools",
+      "ec2:GetSecurityGroupsForVpc",
       "elasticloadbalancing:DescribeLoadBalancers",
       "elasticloadbalancing:DescribeLoadBalancerAttributes",
       "elasticloadbalancing:DescribeListeners",
