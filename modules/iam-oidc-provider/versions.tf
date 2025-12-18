a5edae275c996290ad14e254a4a79286264cbbb3
Bryant Biggs <bryantbiggs@gmail.com>
2025-12-18 13:36:29 -0600

    feat: Add provider meta user-agent

--- modules/iam-oidc-provider/versions.tf
+++ modules/iam-oidc-provider/versions.tf
@@ -4,11 +4,17 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 6.0"
+      version = ">= 6.27"
     }
     tls = {
       source  = "hashicorp/tls"
       version = ">= 3.0"
     }
+  }
+
+  provider_meta "aws" {
+    user_agent = [
+      "github.com/terraform-aws-modules"
+    ]
   }
 }
